46f75c6d9e0a6dc518587b3c6f1fa50ed60a7158
Zuglya <irussel86@gmail.com>
2024-09-20 22:38:21 +0400

    Решено Задание2 Урока7 и Задание со звёздочкой.

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,26 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        //todo Тут увольняем айтишников
+//        for(int id =0; id <= 100; id = id + 2) {
+//            if(id == 0){
+//                continue;
+//            }
+//            System.out.println("Уволен айтишник с id " + id);
+//        }
+//    }
+//}
+
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+        int id = 0;
+        while (id <= 100) {
+            if(id == 0) {
+                id += 2;
+                continue;
+            }
+            System.out.println("Уволен айтишник с id " + id);
+            id += 2;
+        }
     }
 }
